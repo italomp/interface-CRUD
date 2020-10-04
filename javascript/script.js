93f53b50f7878aa3b421017ed0eb2df1338d2dae
italomp <italo.pereira@ccc.ufcg.edu.br>
2020-10-04 17:53:56 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -104,7 +104,7 @@ buttonCreateProduct.addEventListener("click", async function(){
     optRequest = {method: "post",
                   headers: {"Content-type":"application/json",
                             "Authorization":"Bearer " + token,
-                            "Referrer-Policy: "unsafe-url"},
+                            "Referrer-Policy: unsafe-url},
                   credentials: "include",
                   body: JSON.stringify(newProduct)};
     if(token == undefined){
