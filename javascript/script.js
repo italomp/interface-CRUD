3cb6e93e828f56fafe7600a641cef03dc37431b2
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2020-10-13 18:02:14 -0300

    put and delete mothods created-a

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,3 @@
-let buttonUserRegistry = document.getElementById("buttonUserRegistry");
-let buttonUserLogin = document.getElementById("buttonUserLogin");
 let buttonCreateProduct = document.getElementById("buttonCreateProduct");
 let buttonReadProduct = document.getElementById("buttonReadProduct");
 let buttonUpdateProduct = document.getElementById("buttonUpdateProduct");
@@ -7,84 +5,18 @@ let buttonDeleteProduct = document.getElementById("buttonDeleteProduct");
 let result = document.getElementById("result");
 
 /**
- * Esse trecho de código adiciona um evento ao elemento
- * buttonUserRegistry.
+ * Esse trecho de código adiciona um evento ao elemento 'buttonCreateProduct'.
  * 
- * evento: click
+ * evento: click.
  * 
- * callback: função que coleta dados do novo usuario no form com 
- * id "userRegistry" e envia esses dados para p servidor através 
- * de uma requisição POT.
- * Esses daados compõe um novo usuário que será cadastrado no 
- * servidor
+ * callback: função que cria um novo produto a partir dos dados inseridos no form,
+ * envia uma request do tipo post para o servidor e insere o http status da response
+ * no elemento 'result'.
  */
-buttonUserRegistry.addEventListener("click", function(){
-    let form = document.getElementById("userRegistry");
-    let inputName, inputLogin, inputPassword;
-    let newUser, registerUrl, optRequest;
-    for(e of form){
-        if(e.name == "inputName"){
-            inputName = e.value;
-        }
-        else if(e.name == "inputLogin"){
-            inputLogin = e.value;
-        }
-        else if(e.name == "inputPassword"){
-            inputPassword = e.value;
-        }
-    }
-    newUser = {"name": inputName, 
-               "login": inputLogin, 
-               "password": inputPassword};
-    registerUrl = "http://localhost:8080/api/v1/user/";
-    optRequest = {method: "post",
-                  headers: {"Content-type": "application/json"},
-                  body: JSON.stringify(newUser)};
-    fetch(registerUrl,optRequest)
-    .then(resp => result.innerText = resp.status);
-});
-
-/**
- * Esse trecho de código adiciona um evento ao elemento 
- * "buttonUserLogin".
- * 
- * evento: click
- * 
- * callback: função que coleta dados do usuário que vai
- * efetuar login (esses dados estão no form que usa o
- * id "userLogin") e envia uma requisição POST para o
- * servidor.
- * E ainda, armaena o token recebido do servido no
- * localStorage.
- */
-buttonUserLogin.addEventListener("click", function(){
-    let form = document.getElementById("userLogin");
-    let inputLogin, inputPassword;
-    let user, loginUrl, optRequest;
-    for(e of form){
-        if(e.name == "inputLogin"){
-            inputLogin = e.value;
-        }
-        else if(e.name == "inputPassword"){
-            inputPassword = e.value;
-        }
-    }
-    user = {"login": inputLogin, 
-            "password": inputPassword};
-    loginUrl = "http://localhost:8080/api/v1/auth/login";
-    optRequest = {method: "post",
-                  headers: {"Content-type": "application/json"},
-                  body: JSON.stringify(user)};
-    fetch(loginUrl, optRequest)
-    .then(resp => resp.json())
-    .then(tokenObject => window.localStorage.setItem("token", tokenObject.token))
-    .then(result.innerText = "Login realizado com sucesso!");
-});
-
-buttonCreateProduct.addEventListener("click", async function(){
+buttonCreateProduct.addEventListener("click", function(){
     let form = document.getElementById("createProduct");
     let inputName, inputDescription, inputPrice;
-    let newProduct, createProductUrl, optRequest, token;
+    let newProduct, createProductUrl, optRequest;
     for(e of form){
         if(e.name == "inputName"){
             inputName = e.value;
@@ -99,55 +31,90 @@ buttonCreateProduct.addEventListener("click", async function(){
     newProduct = {"name": inputName, 
                   "description": inputDescription, 
                   "price": inputPrice};
-    createProductUrl = "http://localhost:8080/api/v1/product/private/";
-    token = await window.localStorage.getItem("token");
-    optRequest = {method: "post",
-                  headers: {"Content-type":"application/json",
-                            "Authorization":"Bearer " + token},
-                  credentials: "include",
+    createProductUrl = "http://localhost:8080/api/v1/product/";
+    optRequest = {method: "POST",
+                  headers: {"Content-type":"application/json"},
                   body: JSON.stringify(newProduct)};
-    if(token == undefined){
-       console.log("token == undefined");
-    }
-    else if (token == Promise){
-        console.log("token == undefined");
-    }
-    else{
-        console.log(token);
-    }
     fetch(createProductUrl, optRequest)
     .then(resp => {result.innerText = resp.status;});
 });
 
+/**
+ * Esse trecho de código adiciona um evento ao elemento 'buttonReadProduct'.
+ * 
+ * evento: click.
+ * 
+ * calback: função que extrai o inputProductID do form, faz a requisição do tipo get,
+ * converte o objeto json retornado numa string json e adiciona essa string ao
+ * elemento 'result'.
+ */
+buttonReadProduct.addEventListener("click", function(){
+    let form = document.getElementById("readProduct");
+    let indexInputProductID = 0;
+    let inputProductID = form[indexInputProductID].value;
+    let readProductUrl = "http://localhost:8080/api/v1/product/" + inputProductID;
+    fetch(readProductUrl)
+    .then(resp => resp.json())
+    .then(json => result.innerText = JSON.stringify(json));
+});
 
-/*
-let inputName;
-let inputLogin;
-let inputPassword;
-let inputDescription;
-let inputPrice;
-let inputProductID;
-function inputCollector(){
+/**
+ * Esse trecho de código adiciona um evento ao elemento 'buttonUpdateProduct'.
+ * 
+ * evento: click
+ * 
+ * callback: Função que cria a nova versão do produto que será atualizado, a partir
+ * dos inputs fornecidos no form; enviar uma requisição do tipo PUT para o servidor e
+ * e insere o http status da response no elemento 'result'.
+ */
+buttonUpdateProduct.addEventListener("click", function(){
+    let form = document.getElementById("updateProduct");
+    let inputProductID, inputName, inputDescription, inputPrice;
+    let product, createProductUrl, optRequest;
+    for(e of form){
+        if(e.name == "inputProductID"){
+            inputProductID = e.value;
+        }
+        else if(e.name == "inputName"){
+            inputName = e.value;
+        }
+        else if(e.name == "inputDescription"){
+            inputDescription = e.value;
+        }
+        else if(e.name == "inputPrice"){
+            inputPrice = e.value;
+        }
+    }
+    product = {"id": inputProductID,
+                  "name": inputName, 
+                  "description": inputDescription, 
+                  "price": inputPrice};
+    createProductUrl = "http://localhost:8080/api/v1/product/";
+    optRequest = {method: "PUT",
+                  headers: {"Content-type":"application/json"},
+                  body: JSON.stringify(product)};
+    fetch(createProductUrl, optRequest)
+    .then(resp => {result.innerText = resp.status;});
+});
 
-}
-for(e of form){
-    if(e.name == "inputName"){
-        inputName = e.value;
-    }
-    else if(e.name == "inputLogin"){
-        inputLogin = e.value;
-    }
-    else if(e.name == "inputPassword"){
-        inputPassword = e.value;
-    }
-    else if(e.name == "inputDescription"){
-        inputDescription = e.value;
-    }
-    else if(e.name == "inputPrice"){
-        inputPrice = e.value;
-    }
-    else if(e.name == "inputProductID"){
-        inputProductID = e.value;
-    }
-}
-*/
+/**
+ * Esse trecho de código adiciona o um evento ao elemento 'buttonDeleteProduct'.
+ * 
+ * evento: click.
+ * 
+ * callback: Função que pega o inputProductID do form e envia uma requisição do
+ * tipo delete para o servidor e adiciona o http status da resposta no elemento
+ * 'result'.
+ */
+buttonDeleteProduct.addEventListener("click", function(){
+    let form = document.getElementById("deleteProduct");
+    let indexInputProductID = 0;
+    let inputProductID = form[indexInputProductID].value;
+    optRequest = {method: "DELETE",
+                  headers: {"Content-type":"application/json"},};
+    let deleteProductUrl = "http://localhost:8080/api/v1/product/" + inputProductID;
+    fetch(deleteProductUrl, optRequest)
+    .then(resp => {result.innerText = resp.status;});
+
+});
+
