6fbd6dfa893135d979fe80bb1452e0e66df993ca
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2020-10-19 01:03:23 -0300

    implemented features: update, search and delete

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,44 @@
-let createButton = document.getElementsByName("create button")[0];
-let updateButton = document.getElementsByName("update button")[0]; 
-let searchButton = document.getElementsByName("search button")[0];
-let deleteButton = document.getElementsByName("delete button")[0];
+const firstElement = 0;
+const secondElement = 1;
+const thirdElement = 2;
+const fourthElement = 3;
+let createButton = document.getElementsByName("create button")[firstElement];
+let updateButton = document.getElementsByName("update button")[firstElement]; 
+let searchButton = document.getElementsByName("search button")[firstElement];
+let deleteButton = document.getElementsByName("delete button")[firstElement];
 
-/**
- * Esse trecho de código adiciona um evento ao elemento 'createButton'.
- * 
- * evento: click
- * 
- * callback: função que cria um newUser a partir dos dados do formulário,
- * envia uma request do tipo post e altera o elemento 'resultArea' de 
- * acordo com o resultado da response
- */
-createButton.addEventListener("click", function(){
-    let productName, productDescription, productPrice, newProduct;
-    let form = document.getElementsByClassName("formContainer")[0];
-    let createProductUrl, optRequest;
-    let resultArea = document.getElementsByClassName("resultArea")[0];
+//AUXILIARY FUNCTIONS
 
+function addSuccessMessageIntoElement(element, msg){
+    element.innerHTML = msg;
+    element.style.backgroundColor = "#4bb543";
+    element.style.color = "#1f4a1c";
+}
+
+async function addFailMessageIntoElement(element, resp){
+    let err = await resp.json();
+    element.innerHTML = "<h3>" + err.message + "</h3>";
+    element.style.backgroundColor = "#ff1a1a";
+    element.style.color = "white";
+}
+
+async function addProductIntoElement(resp, element){
+    product = await resp.json();
+    let name = product.name;
+    let description = product.description;
+    let price = product.price;
+    
+    element.innerHTML = "<p>" + 
+                        "<strong>" + "Name: " + "</strong>" + name + "<br>" +
+                        "<strong>" + "Description: " + "</strong>" + description + "<br>" +
+                        "<strong>" + "Price: " + "</strong>" + price + 
+                        "<p>";
+    element.style.backgroundColor = "white";
+    element.style.color = "black";
+}
+
+function collectFormData(form, productName, productDescription, 
+                         productPrice){
     for(input of form){
         if(input.name == "product name"){
             productName = input.value;
@@ -29,33 +50,145 @@ createButton.addEventListener("click", function(){
             productPrice == input.value;
         }
     }
-    newProduct = {name: productName,                 
-                  description: productDescription,
-                  price: productPrice };
+}
+
+function collectFormData(form, productID, productName, productDescription, 
+                         productPrice){        
+    for(input of form){
+        if(input.name == "product id"){
+            productID = input.value;
+        }
+        else if(input.name == "product name"){
+            productName = input.value;
+        }
+        else if (input.name == "product description"){
+            productDescription = input.value;
+        }
+        else if(input.name == "product price"){
+            productPrice == input.value;
+        }
+    }
+}
+
+function createProduct(productName, productDescription, productPrice){
+    return {name: productName, 
+            description: productDescription, 
+            prince: productPrice};
+}
+
+function createProduct(ID, productName, productDescription, productPrice){
+    return {id: ID,
+            name: productName, 
+            description: productDescription, 
+            prince: productPrice};
+}
+
+
+
+//EVENTS
+
+/**
+ * Esse trecho de código adiciona um evento ao elemento 'createButton'.
+ * 
+ * Evento: Click.
+ * 
+ * Callback: Função que cria um newUser a partir dos dados do formulário,
+ * envia uma request do tipo post e altera o elemento 'resultArea' de 
+ * acordo com o resultado da response.
+ */
+createButton.addEventListener("click", function(){
+    let productName, productDescription, productPrice, newProduct;
+    let form = document.getElementsByClassName("formContainer")[firstElement];
+    let createProductUrl, optRequest;
+    let resultArea = document.getElementsByClassName("resultArea")[firstElement];
+
+    collectFormData(form, productName, productDescription, productPrice);
+    newProduct = createProduct(productName, productDescription, productPrice);
+
     createProductUrl = "http://localhost:8080/api/v1/product/";
     optRequest = {method: "POST",
                   headers: {"Content-type": "application/json"},
                   body: JSON.stringify(newProduct)};
+
     fetch(createProductUrl, optRequest)
-    .then(resp => addContentAndStyleInResultArea(resultArea, resp));
-});
+    .then(resp => {
+        if(resp.ok){
+            addSuccessMessageIntoElement(resultArea, 
+                "<h3> Product Was Created Successful! </h3>");
+        }
+        else{
+            addFailMessageIntoElement(resultArea, resp);
+        }
+    })
+    .catch(err => console.log("Erro de rede"))});
+
+
 
 /**
- * Essa é uma função auxiliar que adiciona conteúdo e estilo ao
- * elemento 'resultArea' de acordo com a response recebida
+ * Esse trecho de código adiciona um evento ao elemento 'updateButton'.
  * 
- * @param {resultArea} - elemento no qual o conteúdo e o estilo
- * serão inseridos
+ * Evento: click
  * 
- * @param {response} - response recebida do servidor
+ * Callback: Função cria um produto atualizado a partir de dados do form
  */
-function addContentAndStyleInResultArea(resultArea, response){
-    if(response.status == 200){
-        resultArea.innerHTML = "<h3> Product Was Created Successful! </h3>";
-        resultArea.style.backgroundColor = "#4bb543";
-        resultArea.style.color = "#1f4a1c";
-    }
-    else{
-        // nao consegui provocar o erro no servidor
-    }
-}
+updateButton.addEventListener("click", function(){
+    let productID, productName, productDescription, productPrice, updatedProduct;
+    let form = document.getElementsByClassName("formContainer")[secondElement];
+    let updateProductUrl, optRequest;
+    let resultArea = document.getElementsByClassName("resultArea")[secondElement];
+
+    collectFormData(form, productID, productName, productDescription, productPrice);
+    updatedProduct = createProduct(productID, productName, productDescription, productPrice);
+
+    updateProductUrl = "http://localhost:8080/api/v1/product/";
+    optRequest = {method: "PUT",
+                  headers: {"Content-type": "application/json"},
+                  body: JSON.stringify(updatedProduct)};
+    fetch(updateProductUrl, optRequest)
+    .then(resp => {
+        if(resp.ok){
+            addSuccessMessageIntoElement(resultArea, 
+                "<h3> Product Was Updated Successful! </h3>");
+        }
+        else{
+            addFailMessageIntoElement(resultArea, resp);
+        }
+    })
+    .catch(err => console.log("Erro de rede"));
+});
+
+
+searchButton.addEventListener("click", function(){
+    let form = document.getElementsByClassName("formContainer")[thirdElement];
+    let productID = form[firstElement].value;
+    let resultArea = document.getElementsByClassName("resultArea")[thirdElement];
+    let searchProductUrl = "http://localhost:8080/api/v1/product/" + productID;
+    fetch(searchProductUrl)
+    .then(resp => {
+        if(resp.ok){
+            addProductIntoElement(resp, resultArea);
+        }
+        else{
+            addFailMessageIntoElement(resultArea, resp);
+        }
+    });
+});
+
+
+deleteButton.addEventListener("click", function(){
+    let form = document.getElementsByClassName("formContainer")[fourthElement];
+    let productID = form[firstElement].value;
+    let resultArea = document.getElementsByClassName("resultArea")[fourthElement];
+    let searchProductUrl = "http://localhost:8080/api/v1/product/" + productID;
+    let optRequest = {method: "DELETE"};
+    fetch(searchProductUrl, optRequest)
+    .then(resp => {
+        if(resp.ok){
+            addSuccessMessageIntoElement(resultArea, 
+                "<h3> Product Was Deleted Successful! </h3>");
+        }
+        else{
+            addFailMessageIntoElement(resultArea, resp);
+        }
+    });
+});
